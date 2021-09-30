4d5a055db44f823da422fdf63943f76e9d117042
AngelinesYG <79059491+AngelinesYG@users.noreply.github.com>
2021-09-30 11:24:50 -0400

    getting started with nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <div className="nav-container">
+                <nav>
+                    <h1 onClick={() => window.location.replace("/#nav-container")}>Septima Trompeta</h1>
+                    <ul>
+                        <a onClick={() => window.location.replace("/#about-container")}><li>Nuestra Mision</li></a>
+                        <a onClick={() => window.location.replace("/#staff-container")}><li>Quienes Somos</li></a>
+                        <a onClick={() => window.location.replace("/#events-container")}><li>Eventos</li></a>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Nav
